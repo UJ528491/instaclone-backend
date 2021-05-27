a1c25aca577bf3d47e3dc65890fe03dd81ed7d40
UJ528491 <hyj0829@gmail.com>
2021-05-28 01:57:23 +0900

    add login part

--- users/users.mutation.js
+++ users/users.mutation.js
@@ -8,30 +8,59 @@ export default {
       { firstName, lastName, username, email, password }
     ) => {
       //  check if username or email are already on DB.
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          console.log(existingUser);
+          throw new Error("This username or password is already taken.");
+        }
+        //  hash password
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    //  save and return the user
+    login: async (_, { username, password }) => {
+      //find user with args.username
+      const user = await client.user.findFirst({
+        where: { username },
       });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
-      //  hash password
-      //  save and return the user
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      //check password with args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Invalid Password",
+        };
+      }
+      //issue a token and send it
     },
   },
 };
