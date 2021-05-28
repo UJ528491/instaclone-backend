2e986e5cd32a8dde2597bd2c23761cc4e86ee548
UJ528491 <hyj0829@gmail.com>
2021-05-29 01:51:28 +0900

    protect resolvers by curring

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser, protectResolver } from "./users/users.util";
 
 const server = new ApolloServer({
   schema,
+  context: async ({ req }) => {
+    return {
+      logedInUser: await getUser(req.headers.token),
+      protectResolver,
+    };
+  },
 });
 const PORT = process.env.PORT;
 
 server.listen(PORT).then(() => {
-  console.log(`server is running on http://localhost:${PORT}/`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}/✅`);
 });
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,39 +1,41 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import client from "../../client";
+import { protectResolver } from "../users.util";
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword, token }
-    ) => {
-      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
+    editProfile: protectResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { logedInUser }
+      ) => {
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where: {
+            id: logedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update profile",
+          };
+        }
       }
-      const updatedUser = await client.user.update({
-        where: {
-          id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update profile",
-        };
-      }
-    },
+    ),
   },
 };
